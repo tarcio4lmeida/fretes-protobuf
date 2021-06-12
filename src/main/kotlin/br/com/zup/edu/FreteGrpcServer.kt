14213c233a98c1d4e07be0db68aef51c4cdc9015
Tarcio Almeida <tarcioalmeidalima@gmail.com>
2021-06-12 00:05:19 -0300

    Expondo dados via GRPC

---
+++ src/main/kotlin/br/com/zup/edu/FreteGrpcServer.kt
@@ -0,0 +1,27 @@
+package br.com.zup.edu
+
+import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class FreteGrpcServer : FretesServiceGrpc.FretesServiceImplBase()  {
+
+    private val logger = LoggerFactory.getLogger(FretesServiceGrpc::class.java)
+
+    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+
+        logger.info("Calculando frete para request: $request")
+
+        val response = CalculaFreteResponse.newBuilder()
+            .setCep(request!!.cep)
+            .setValor(Random.nextDouble(from = 0.0, until = 140.0))
+            .build()
+
+        logger.info("Frete calculado: $response")
+
+        responseObserver!!.onNext(response)
+        responseObserver.onCompleted()
+    }
+}
